1d69618a01b948e7575bd2e8c8d7dba785a0ea5a
Никита <mnd1work@gmail.com>
2023-03-06 19:48:35 +0400

    commit 6.3.23 (2)

--- Taxi_Depot.DAL/MainContext.cs
+++ Taxi_Depot.DAL/MainContext.cs
@@ -15,7 +15,6 @@ namespace Taxi_Depot.DAL
         public DbSet<Order> Orders { get; set; }
         public DbSet<Driver> Drivers { get; set; }
         public DbSet<Transport> Transports { get; set; }
-        public DbSet<Service> Service { get; set; }
 
 
         public MainContext(DbContextOptions<MainContext> options) : base(options)
@@ -29,16 +28,27 @@ namespace Taxi_Depot.DAL
 
             modelBuilder.Entity<Driver>(entity =>
             {
-                entity.HasIndex(n => n.Id).IsUnique();
                 entity.HasData(new List<Driver>()
                 {
                     new Driver() {Id = 1, Name = "Никита", Surname = "Мальцев", Phone = "+79896817945"},
                     new Driver() {Id = 2, Name = "Александр", Surname = "Копаев", Phone= "+79378286840"},
                 });
+            });
 
+            modelBuilder.Entity<Order>(entity =>
+            {
                 entity.HasData(new List<Order>()
                 {
                     new Order() {Id = 1, FullName = "Арнаутов Иван Павлович", ClientPhone = "+79370590439", Status = OrderStatusEnum.Waiting, From = "наб. Сталина, 88", Where = "пл. Домодедовская, 17", Cost = 600},
+                    new Order() {Id = 2, FullName = "Марков Евгений Максимович", ClientPhone = "+79371635151", Status = OrderStatusEnum.Delivered, From = "пл. Будапештсткая, 90", Where = "шоссе Славы, 92", Cost = 580},
+                    new Order() {Id = 3, FullName = "Макаров Егор Александрович", ClientPhone = "+79373022327", Status = OrderStatusEnum.Waiting, From = "спуск Чехова, 99", Where = "пр. Бухарестская, 98", Cost = 531},
+                    new Order() {Id = 4, FullName = "Шульгин Арсений Михайлович", ClientPhone = "+79376235721", Status = OrderStatusEnum.Canceled, From = "проезд Гагарина, 26", Where = "шоссе 1905 года, 35", Cost = 406},
+                    new Order() {Id = 5, FullName = "Смирнов Максим Георгиевич", ClientPhone = "+79371508088", Status = OrderStatusEnum.Canceled, From = "въезд Славы, 67", Where = "пл. Гагарина, 48", Cost = 202},
+                    new Order() {Id = 6, FullName = "Суслова Александра Львовна", ClientPhone = "+79374186791", Status = OrderStatusEnum.Delivered, From = "спуск Домодедовская, 56", Where = "въезд Ленина, 69", Cost = 556},
+                    new Order() {Id = 7, FullName = "Меркулов Максим Никитич", ClientPhone = "+79370334512", Status = OrderStatusEnum.Canceled, From = "въезд Славы, 26", Where = "шоссе Славы, 94", Cost = 557},
+                    new Order() {Id = 8, FullName = "Фролова Виктория Максимовна", ClientPhone = "+79373681997", Status = OrderStatusEnum.Canceled, From = "пл. Ладыгина, 15", Where = "шоссе Ладыгина, 13", Cost = 398},
+                    new Order() {Id = 9, FullName = "Шубин Михаил Васильевич", ClientPhone = "+79374895787", Status = OrderStatusEnum.OnMove, From = "ул. Бухарестская, 70", Where = "спуск Славы, 52", Cost = 281},
+                    new Order() {Id = 10, FullName = "Егоров Дмитрий Артёмович", ClientPhone = "+79370687599", Status = OrderStatusEnum.Delivered, From = "пл. Гагарина, 53", Where = "пер. Славы, 65", Cost = 493},
                 });
             });
         }
--- Taxi_Depot/Controllers/OrdersController.cs
+++ Taxi_Depot/Controllers/OrdersController.cs
@@ -5,6 +5,8 @@ using System.Collections.Generic;
 using System.Diagnostics;
 using System.Linq;
 using System.Threading.Tasks;
+using Taxi_Depot.DAL;
+using Taxi_Depot.DAL.Enums;
 using Taxi_Depot.Models;
 
 namespace Taxi_Depot.Controllers
@@ -12,20 +14,44 @@ namespace Taxi_Depot.Controllers
     public class OrdersController : Controller
     {
         private readonly ILogger<OrdersController> _logger;
+        private readonly MainContext _mainContext;
+        private static int EditId;
 
-        public OrdersController(ILogger<OrdersController> logger)
+        public OrdersController(ILogger<OrdersController> logger, MainContext mainContext)
         {
             _logger = logger;
+            _mainContext = mainContext;
         }
 
         public IActionResult Index()
         {
-            return View();
+            var res = _mainContext.Orders.OrderBy(x => x.Status).ToList();
+
+            return View(res);
         }
 
-        public IActionResult Privacy()
+        public IActionResult EditOrder(int id) 
         {
-            return View();
+            var res = _mainContext.Orders.Where(x => x.Id == id).ToList();
+            EditId = id;
+
+            return View(res);
+        }
+
+        [HttpPost]
+        public RedirectResult SendOrder(string FullName, string ClientPhone, string From, string Where, int Cost)
+        {
+            var res = _mainContext.Orders.FirstOrDefault(x => x.Id == EditId);
+
+            res.FullName = FullName;
+            res.ClientPhone = ClientPhone;
+            res.From = From;
+            res.Where = Where;
+            res.Cost = Cost;
+
+            _mainContext.SaveChanges();
+
+            return Redirect("/Orders");
         }
 
         [ResponseCache(Duration = 0, Location = ResponseCacheLocation.None, NoStore = true)]
